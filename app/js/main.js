2cc137f802e01d3689dff746cbdb79b226386784
Valeriy <leshikzoloto@gmail.com>
2021-10-14 18:52:12 +0300

    finish desctop-start mobile

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-$(function () {});
+$(function () {
+	//авто-закрытие мобильного меню
+	$(".burger, .menu__item a").on("click", function () {
+		$(".menu").toggleClass("menu--active"),
+			$(".burger").toggleClass("burger--active");
+	});
+});
