78c23845e9bd384a553dec5e0187d48cba4fa236
Valeriy <leshikzoloto@gmail.com>
2021-10-15 19:32:00 +0300

    mobile-header-footer-team

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,17 @@ $(function () {
 		$(".menu").toggleClass("menu--active"),
 			$(".burger").toggleClass("burger--active");
 	});
+
+	$(".scroll").click(function (event) {
+		console.log(11111);
+		event.preventDefault();
+		$("html").animate({ scrollTop: 0 }, 1500);
+	});
+
+	$(".menu a, header a").on("click", function (e) {
+		e.preventDefault();
+		const id = $(this).attr("href"),
+			top = $(id).offset().top;
+		$("body,html").animate({ scrollTop: top }, 1500);
+	});
 });
